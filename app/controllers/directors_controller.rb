735cd30790f90d57b93d1faae93222924bd48670
juliang2127 <164977539+juliang2127@users.noreply.github.com>
2024-05-02 06:30:27 +0000

    done

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,4 +1,42 @@
 class DirectorsController < ApplicationController
+  def create
+    m = Director.new
+    m.name = params.fetch("director_name")
+    m.dob = params.fetch("director_dob")
+    m.bio = params.fetch("director_bio")
+    m.image = params.fetch("director_image")
+
+    m.save
+
+    redirect_to("/directors")
+    
+  end
+
+  def destroy
+    the_id = params.fetch("an_id")
+    matching_record = Director.where({ :id => the_id})
+    the_director = matching_record.at(0)
+    the_director.destroy
+
+    redirect_to("/directors")
+  end
+
+  def edit
+    m_id = params.fetch("the_id")
+
+    matching_record = Director.where({ :id => m_id})
+    m = matching_record.at(0)
+
+    m.name = params.fetch("director_name")
+    m.dob = params.fetch("director_dob")
+    m.bio = params.fetch("director_bio")
+    m.image = params.fetch("director_image")
+
+    m.save
+
+    redirect_to("/directors/#{m.id}")
+  end
+  
   def index
     matching_directors = Director.all
     @list_of_directors = matching_directors.order({ :created_at => :desc })
